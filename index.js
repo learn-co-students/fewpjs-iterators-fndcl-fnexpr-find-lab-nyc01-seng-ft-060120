b664594fb8cd0f1d365ad8c9f9bf603103df2acb
liam <leamsi.esc@gmail.com>
2020-07-19 12:30:41 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayOfObjects) {
+  let winner = arrayOfObjects.find((object) => {return object.result === "W"});
+  return (winner) ? winner.year : undefined;
+}
