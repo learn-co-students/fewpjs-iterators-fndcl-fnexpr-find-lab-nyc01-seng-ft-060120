5f988f864bdd02d88a1484d22768012d176d5b4a
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-24 11:21:07 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winner = array.find(team => team.result=== "W")
+  return winner ? winner.year : undefined
+}
